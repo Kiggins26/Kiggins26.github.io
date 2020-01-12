0d5ada935a2f22beee142168fe27e35420188a5f
Chinmay Gopal <47234753+ChinmayGopal931@users.noreply.github.com>
2020-01-11 22:20:58 -0800

    Add files via upload

--- Starter.py
+++ Starter.py
@@ -1,5 +1,3 @@
-#get initial image
-# ask user and get image into the inital image directory in the same folder
 import io
 import os
 # Imports the Google Cloud client library
@@ -9,38 +7,41 @@ import user_scraper
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\chinm\\PycharmProjects\\NwHacksTest1\\vision.json"
 label_list = []
 
-def starter_image(imgDest):
-        client = vision.ImageAnnotatorClient()
-        #dir = "C:\\Users\\chinm\\PycharmProjects\\NwHacksTest1\\InitalPic"
-        list = os.listdir(dir)  # dir is your directory path
-        numFiles = len(list)
-        file_name = os.path.abspath(imgDest)
+def starter_images(imgDest):
+            client = vision.ImageAnnotatorClient()
 
-        with io.open(file_name, 'rb') as image_file:
-            content = image_file.read()
+     #   dir = "C:\\Users\\chinm\\PycharmProjects\\NwHacksTest1\\InitalPic"
+     #   list = os.listdir(dir)  # dir is your directory path
+     #   numFiles = len(list)
+     #   for i in list:
+     #       file_name = os.path.abspath("C:\\Users\\chinm\\PycharmProjects\\NwHacksTest1\\InitalPic\\"+i)
 
-        image = types.Image(content=content)
-        response = client.label_detection(image=image)
-        labels = response.label_annotations
-        client = vision.ImageAnnotatorClient()
-        objects = client.object_localization(image=image).localized_object_annotations
+            file_name=imgDest
+            with io.open(file_name, 'rb') as image_file:
+                content = image_file.read()
 
-        for object_ in objects:
-            print(object_.name)
-            label_list.append(object_)
+            image = types.Image(content=content)
+            response = client.label_detection(image=image)
+            labels = response.label_annotations
+            client = vision.ImageAnnotatorClient()
+            objects = client.object_localization(image=image).localized_object_annotations
 
-        print('Labels:')
-        for label in labels:
-            print(label.description)
-            label_list.append(label.description)
+            for object_ in objects:
+                print(object_.name)
+                label_list.append(object_)
 
-        print("-----------------------------------------------------------------------------------------------------------------------------")
+            print('Labels:')
+            for label in labels:
+                label_list.append(label.description)
+
+            print("-----------------------------------------------------------------------------------------------------------------------------")
+            return(label_list)
 
 
+starter_images("C:\\Users\\chinm\\PycharmProjects\\NwHacksTest1\\Images\\3865022_fpx.jfif")
 
-# starter_image()
 extra_labels=[]
 for i in label_list:
     extra_labels.append(user_scraper.get_users_by_tag(i))
 
-print(extra_labels)
+print(extra_labels) 
