5114a5fb30f8276bf48ee3f11cc193cbb7c2790a
Yohen Thounaojam <yohenthounaojam@gmail.com>
2020-01-11 17:06:31 -0800

    Image import with image destination in variable:imageDest

--- GUI.py
+++ GUI.py
@@ -1,23 +1,36 @@
 from tkinter import *
-
+from tkinter import filedialog
 root = Tk()
+root.title("Target Market")
 theLabel = Label(root, text="Get a target market to advertise your products.", bg="black", fg="white")
-theLabel.pack(fill=X)
+theLabel.grid(row=0,columnspan=3)
 
-#-----------------------FRAMES-----------------------------------------------
-topframe = Frame(root)
-topframe.pack()
-middleframe = Frame(root)
-middleframe.pack()
-bottomframe = Frame(root)
-bottomframe.pack(side=BOTTOM)
+#------------------------FUNCTIONS-------------------------------------------
+def fileOpen():
+    imageDest=filedialog.askopenfile()
+    message = Label(text=imageDest, fg="grey").grid(row=5, columnspan=4)
 
+#------------------------INPUT-----------------------------------------------
+budgetLabel=Label(root, text="Enter your budget in CAD")
+budgetLabel.grid(row=3)
+
+budget=Entry(root)
+budget.grid(row=3, column=3)
+#-----------------------------------
+budgetLabel=Label(root, text="Select your product image")
+budgetLabel.grid(row=4)
+
+buttonInsert = Button(text="INSERT", command=fileOpen)
+buttonInsert.grid(row=4, column=3)
+#------------------------------------
+buttonSubmit = Button(text="SUBMIT", bg="black", fg="white")
+buttonSubmit.grid(row=6, columnspan=4)
 #------------------------Buttons----------------------------------------------
-buttonSubmit = Button(middleframe, text="Submit")
-buttonHelp = Button(middleframe, text="Help")
-
-buttonSubmit.pack(side=LEFT)
-buttonHelp.pack(side=LEFT)
+# buttonSubmit = Button(middleframe, text="Submit")
+# buttonHelp = Button(middleframe, text="Help")
+#
+# buttonSubmit.pack(side=LEFT)
+# buttonHelp.pack(side=LEFT)
 
 root.mainloop()
 
