5dbbdac0d39eccea7ccc6096914c9133617b2f05
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2020-01-12 11:35:20 -0800

    Update InfoPro.py

--- InfoPro.py
+++ InfoPro.py
@@ -4,9 +4,6 @@ def strToFloat(userList):
     holderDic = {}
     for i in userList:
         for x in i:
-            print(x)
-            print("\n")
-            print(i.get(x))
             if "k" in i.get(x):
                 value = i.get(x).replace("k", "")
                 holderDic[x] = (float(value)*1000)
@@ -46,4 +43,4 @@ def getOptFollowerRange(price,userList):
     return userList
 pricetest = 250000
 testlist = [[{'astral_selection': '61 ', 'rojasmanuel': '2,658 ', 'musicalsoul_vishal': '2,739 ', 'theaniketkaljunkar': '2,397 ', 'zantoutphotography': '2,500 ', 'm.sophie_karl': '147 ', 'txfuchxnn': '1,609 ', 'lastribotello43': '0 ', 'elizabethplus2dogs': '285 ', 'rosalie_portfolio': '73 ', 'asajjjj___': '37 ', 'rajicc_': '368 ', 'matejkubela': '491 ', 'burylova_olesya': '28.3k ', 'criaetif': '26 ', 'juliawphotography': '186 ', 'singhsandeep8312': '386 ', 'pumba_photo.studio': '176 ', 'smileplzz13': '3,134 ', 'dylan_officiel__': '1,449 ', 'gagen_sergei_photo': '105 ', 'paulgruber_arch': '42 ', 'billie_eilish_blog': '8,022 ', 'yairelav98': '1,515 ', 'anastasia_skreminska': '4,178 ', 'krissstina_d': '4,057 '}]]
-print(strToFloat(testlist[0]))
+print(getOptFollowerRange(25000,strToFloat(testlist[0])))
